4b01da43abe9632cecf9d4b225a76ea920446828
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-02 16:39:58 +0600

    52-7 (Recap) Dynamic route, Dynamic Data loader and active route

--- explore-routes/src/components/Header/Header.js
+++ explore-routes/src/components/Header/Header.js
@@ -1,15 +1,60 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="header">
-      <Link to="/home">Home</Link>
-      <Link to="/products">Products</Link>
-      <Link to="/friends">Friends</Link>
-      <Link to="/posts">Posts</Link>
-      <Link to="/about">About</Link>
+      <NavLink
+        to="/home"
+        style={({ isActive }) => {
+          return {
+            color: isActive ? "red" : null,
+          };
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/products"
+        style={({ isActive }) => {
+          return {
+            color: isActive ? "red" : null,
+          };
+        }}
+      >
+        Products
+      </NavLink>
+      <NavLink
+        to="/friends"
+        style={({ isActive }) => {
+          return {
+            color: isActive ? "red" : null,
+          };
+        }}
+      >
+        Friends
+      </NavLink>
+      <NavLink
+        to="/posts"
+        style={({ isActive }) => {
+          return {
+            color: isActive ? "red" : null,
+          };
+        }}
+      >
+        Posts
+      </NavLink>
+      <NavLink
+        to="/about"
+        style={({ isActive }) => {
+          return {
+            color: isActive ? "red" : null,
+          };
+        }}
+      >
+        About
+      </NavLink>
     </div>
   );
 };
